f0e6b6862dad7c2d541fa2c6cf3e61ad623ca1ee
SergeyErshov <s.ershov.shoko@gmail.com>
2023-01-22 11:59:55 +0300

    add logging to script

--- myscript.sh
+++ myscript.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# get current date/time on executing script
+
+now=$(date +"%Y-%m-%d %H:%M:%S")
+
 # Ask for user to enter number
 echo "enter anything number"
 read num1
@@ -7,3 +11,7 @@ read num1
 num2=$(( $num1*2 ))
 
 echo "The double number is $num2"
+
+# write log file
+
+echo '[ {"executing time": "'$now'", "inputed number": "'$num1'"} ]' | jq '.[]' >> /var/log/myscript.log
